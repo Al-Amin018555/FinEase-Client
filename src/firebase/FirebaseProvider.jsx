c38f48d40ec8a12cd683d9e895f1933c372e7f61
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-03-03 16:14:42 +0600

    login with google implemented

--- src/firebase/FirebaseProvider.jsx
+++ src/firebase/FirebaseProvider.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
 import { AuthContext } from '../context/AuthContext';
 import { auth } from '../../firebase.init';
 import { useEffect, useState } from 'react';
@@ -7,11 +7,17 @@ import Loader from '../components/Header/Loader';
 const FirebaseProvider = ({ children }) => {
     const [user, setUser] = useState();
     const [loading, setLoading] = useState(true);
+
+    const googleProvider = new GoogleAuthProvider();
+
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
     const logIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
+    }
+    const googleLogIn = () => {
+        return signInWithPopup(auth, googleProvider);
     }
     const logOut = () => {
         return signOut(auth);
@@ -25,6 +31,7 @@ const FirebaseProvider = ({ children }) => {
     const authInfo = {
         createUser,
         logIn,
+        googleLogIn,
         logOut,
         profileUpdate,
         user,
