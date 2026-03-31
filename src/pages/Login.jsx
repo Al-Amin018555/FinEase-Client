8e45243e93d1117d1b688b7f30a57eeb7fa84556
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-03-31 21:30:34 +0600

    updated login descriptions

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,12 +45,12 @@ const Login = () => {
     }
     return (
         <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+            <div className="hero-content flex-col lg:flex-row-reverse lg:w-7xl">
+                <div className="text-center w-full lg:text-left">
+                    <h1 className="text-5xl font-bold text-base-content">Stay in Control of Your Money</h1>
+                    <p className="py-6 text-2xl text-base-content/70">
+                        Your financial journey continues here. <br />
+                        Track, plan, and grow your wealth with FinEase.
                     </p>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
